5dfe0f4c7b30f10fcba3c9d57175c88d0d057f51
denmigda <denis.migdal@uca.fr>
2026-06-24 14:48:15 +0200

    remove style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,3 @@
 import buildConfigs from "./build/WebpackFramework/index.js";
 
 export default buildConfigs();
-
-import { createRequire } from "node:module";
-
-const require = createRequire(import.meta.url);
-
-console.log(
-  "TS utilisé par webpack:",
-  require.resolve("typescript"),
-  require("typescript").version
-);
