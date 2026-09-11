43ce9d6d3add7f9625d74c3d279f1019d5927c81
denmigda <denis.migdal@uca.fr>
2026-09-11 10:48:40 +0200

    Force font for tests

--- src/pages/tests/fonts/index.ts
+++ src/pages/tests/fonts/index.ts
@@ -1,20 +1,14 @@
 const output = document.querySelector<HTMLElement>(".results")!;
 const unicode = document.querySelector<HTMLElement>(".unicode")!;
-const variants = document.querySelector<HTMLElement>(".variants")!;
+
+const input = document.querySelector<HTMLInputElement>(".input")!;
 
 const variantsCodes = [14,15];
 
-document.querySelector(".symbols")!.addEventListener("click", (ev) => {
+function showSymbol(symbol: string) {
 
-    if( ! (ev.target instanceof HTMLSpanElement) )
-        return;
-
-    const symbol = ev.target.textContent!;
-
-    const results = new Array<HTMLElement>(variantsCodes.length);
+    let variants = "";
     for (let i = 0; i < variantsCodes.length; ++i) {
-
-        const span = results[i] = document.createElement("span");
 
         const base = [...symbol].filter(cp => {
             const code = cp.codePointAt(0)!;
@@ -25,10 +19,21 @@ document.querySelector(".symbols")!.addEventListener("click", (ev) => {
             );
         }).join("");
 
-        span.textContent = base + String.fromCodePoint(0xFE00 + variantsCodes[i]);
+        variants += base + String.fromCodePoint(0xFE00 + variantsCodes[i]);
     }
-    variants.replaceChildren(...results);
 
     unicode.textContent = symbol.codePointAt(0)!.toString(16).toUpperCase().padStart(4, "0");
-    output.style.setProperty("--symbol", `"${symbol}"`);
+    output.style.setProperty("--symbol", `"${variants}"`);
+}
+
+input.addEventListener("input", () => {
+    showSymbol(input.value)
+})
+
+document.querySelector(".symbols")!.addEventListener("click", (ev) => {
+
+    if( ! (ev.target instanceof HTMLSpanElement) )
+        return;
+
+    showSymbol(ev.target.textContent!);
 });
